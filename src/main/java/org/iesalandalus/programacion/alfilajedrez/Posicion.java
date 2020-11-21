0daba9d3a7e449c37c6bbea34b6fc31c62d2ddd5
annaz <annaz@192.168.5.241>
2020-11-21 21:49:59 +0100

    He creado metodos get y set para los atributos, con lanzamiento de excepciones en caso de que se salgan de los parámetros establecidos.

--- src/main/java/org/iesalandalus/programacion/alfilajedrez/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/alfilajedrez/Posicion.java
@@ -4,5 +4,34 @@ public class Posicion {
 	private int fila;
 	private char columna;
 	
+	public int getFila() {
+		return fila;
+	}
+	
+	public Posicion (int fila, char columna) {
+		setFila(fila);
+		setColumna(columna);
+	}
+	
+	private void setFila(int fila) {
+		if (fila < 1 || fila > 8) {
+ 			throw new IllegalArgumentException("ERROR: Fila no válida.");
+		}
+			
+		this.fila = fila;
+	}
+	
+	public char getColumna() {
+		return columna;
+	}
+	
+	private void setColumna(char columna) {
+		if (columna < 'a' || columna > 'h') {
+			throw new IllegalArgumentException("ERROR: Columna no válida.");
+		}
+			
+		this.columna = columna;
+	}
+	
 
 }
