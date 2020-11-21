53fade1cd9357fa969c552fe4c1db87e72892a08
annaz <annaz@192.168.5.241>
2020-11-21 23:31:57 +0100

    He creado los metodos equals y hashCode.

--- src/main/java/org/iesalandalus/programacion/alfilajedrez/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/alfilajedrez/Posicion.java
@@ -1,5 +1,7 @@
 package org.iesalandalus.programacion.alfilajedrez;
 
+import java.util.Objects;
+
 public class Posicion {
 	private int fila;
 	private char columna;
@@ -37,5 +39,28 @@ public class Posicion {
 		this.columna = posicion.getColumna();
 		this.fila = posicion.getFila();
 	}
+
+
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(columna, fila);
+	}
+
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (!(obj instanceof Posicion)) {
+			return false;
+		}
+		Posicion other = (Posicion) obj;
+		return columna == other.columna && fila == other.fila;
+	}
+	
+	
 
 }
