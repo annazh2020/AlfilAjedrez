f8aaabb96c39e88cc05628c2c4d91daa1f7ae2e1
annaz <annaz@192.168.5.241>
2020-11-21 21:58:55 +0100

    He creado constructor con parametros fila y columna

--- src/main/java/org/iesalandalus/programacion/alfilajedrez/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/alfilajedrez/Posicion.java
@@ -8,16 +8,12 @@ public class Posicion {
 		return fila;
 	}
 	
-	public Posicion (int fila, char columna) {
-		setFila(fila);
-		setColumna(columna);
-	}
+	
 	
 	private void setFila(int fila) {
 		if (fila < 1 || fila > 8) {
  			throw new IllegalArgumentException("ERROR: Fila no válida.");
-		}
-			
+		}	
 		this.fila = fila;
 	}
 	
@@ -28,10 +24,16 @@ public class Posicion {
 	private void setColumna(char columna) {
 		if (columna < 'a' || columna > 'h') {
 			throw new IllegalArgumentException("ERROR: Columna no válida.");
-		}
-			
+		}	
 		this.columna = columna;
 	}
+
+	public Posicion(int fila, char columna) {
+		super();
+		this.fila = fila;
+		this.columna = columna;
+	}
+	
 	
 
 }
