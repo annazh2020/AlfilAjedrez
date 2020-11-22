955fb0c058d742b14254fb64bff8e4c70f88375d
annaz <annaz@192.168.5.241>
2020-11-22 20:36:50 +0100

    He añadido lanzamiento de la excepcion NullPointerExcepcion.

--- src/main/java/org/iesalandalus/programacion/alfilajedrez/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/alfilajedrez/Posicion.java
@@ -5,28 +5,26 @@ import java.util.Objects;
 public class Posicion {
 	private int fila;
 	private char columna;
-	
+
 	public int getFila() {
 		return fila;
 	}
-	
-	
-	
+
 	private void setFila(int fila) {
 		if (fila < 1 || fila > 8) {
- 			throw new IllegalArgumentException("ERROR: Fila no válida.");
-		}	
+			throw new IllegalArgumentException("ERROR: Fila no válida.");
+		}
 		this.fila = fila;
 	}
-	
+
 	public char getColumna() {
 		return columna;
 	}
-	
+
 	private void setColumna(char columna) {
 		if (columna < 'a' || columna > 'h') {
 			throw new IllegalArgumentException("ERROR: Columna no válida.");
-		}	
+		}
 		this.columna = columna;
 	}
 
@@ -34,12 +32,14 @@ public class Posicion {
 		setFila(fila);
 		setColumna(columna);
 	}
-	
-	public Posicion (Posicion posicion) {
-		setColumna (posicion.getColumna());
-		setFila (posicion.getFila());
-	}
 
+	public Posicion(Posicion posicion) {
+		if (posicion == null) {
+			throw new NullPointerException("ERROR: No es posible copiar una posición nula.");
+		}
+		this.columna = posicion.getColumna();
+		this.fila = posicion.getFila();
+	}
 
 
 	@Override
