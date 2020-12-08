e4fe8b72d513d76979610f078dabcca7151434e4
annaz <annaz@192.168.5.241>
2020-12-08 18:49:08 +0100

    He realizado el punto 19.1 crear metodo Mostrar Alfil por defecto

---
+++ src/main/java/org/iesalandalus/programacion/alfilajedrez/MainApp.java
@@ -0,0 +1,26 @@
+package org.iesalandalus.programacion.alfilajedrez;
+
+import org.iesalandalus.programacion.utilidades.Entrada;
+
+public class MainApp {
+
+	public MainApp() {
+		
+	}
+	
+	public static void mostrarAlfil() {
+		Alfil alfil1=new Alfil();
+		System.out.println("El alfil mostrado por defecto es:" +alfil1);
+		
+	}
+
+
+	
+	
+public static void main(String[] args) {
+	
+	MainApp.mostrarAlfil();
+	
+
+	}
+}
