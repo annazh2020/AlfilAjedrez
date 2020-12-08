55bae8b6b00e82b2ab90e00a087c882a107afc64
annaz <annaz@192.168.5.241>
2020-12-08 18:55:05 +0100

    He realizado el punto 19.2, creación del método Mostrar Menu

--- src/main/java/org/iesalandalus/programacion/alfilajedrez/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/alfilajedrez/MainApp.java
@@ -11,7 +11,15 @@ public class MainApp {
 	public static void mostrarAlfil() {
 		Alfil alfil1=new Alfil();
 		System.out.println("El alfil mostrado por defecto es:" +alfil1);
-		
+	}
+	
+	public static void mostrarMenu() {
+		System.out.println("Opción 1: Crear un alfil por defecto.");
+		System.out.println("Opción 2: Crear un alfil de un color.");
+		System.out.println("Opción 3: Crear un alfil de un color y columna inicial dada.");
+		System.out.println("Opción 4: Mover el alfil.");
+		System.out.println("Opción 5: Salir.");
+
 	}
 
 
@@ -20,6 +28,10 @@ public class MainApp {
 public static void main(String[] args) {
 	
 	MainApp.mostrarAlfil();
+	
+	MainApp.mostrarMenu();
+	
+	
 	
 
 	}
